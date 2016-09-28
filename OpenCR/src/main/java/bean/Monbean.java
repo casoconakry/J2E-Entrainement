d7f7dff4f606bcfe333ce2aa2208c84904be6b17
caso224 <casocamara@hotmail.com>
2016-09-29 00:28:06 +0200

    Fct: Bean servant d'example

---
+++ OpenCR/src/main/java/bean/Monbean.java
@@ -0,0 +1,39 @@
+package bean;
+
+/**
+ * Created by casoc on 25/09/2016.
+ */
+public class Monbean {
+
+    private String nom;
+    private String prenom;
+    private String genius;
+
+    public Monbean()
+    {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getGenius() {
+        return genius;
+    }
+
+    public void setGenius(String genius) {
+        this.genius = genius;
+    }
+}
